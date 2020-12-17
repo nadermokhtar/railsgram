d7703033755ab025e4d27ce6c448530296658b31
nadermokhtar <nader.mokhtar@gmail.com>
2020-12-17 08:30:56 -0500

    add active storage and avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,23 @@
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
     {
-      success: 'alert-success',
-      error: 'alert-danger',
-      alert: 'alert-warning',
-      notice:'alert-info',
-  }.stringify_keys[flash_type.to_s] || flash_type.to_s
+    success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    notice:'alert-info'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def avatar_url_for(user, options={})
+    size = options[:size] || 32
+
+    if user.avatar.attached?
+      user.avatar.variant(
+        resize: "#{size}x#{size}!"
+      )
+    else
+      hash = Digest::MD5.hexdigest(user.email.downcase)
+      "https://secure.gravatar.com/avatar/#{hash}.png?s=#{size}"
+    end
   end
 end
